ecc3a66213d35e3f405eb7d7123f7473d6b101ee
Bruno Tejeria <brunotejeria@hotmail.com>
2024-03-11 08:40:02 -0300

    Poner en desuso base models y archivo para inicialización

--- utils/schemas/Selenium.py
+++ utils/schemas/Selenium.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 from typing import Literal
 
+@DeprecationWarning
 class LogSettings(BaseModel):
     log_level: Literal["info", "warning", "debug", "error"]
 
+@DeprecationWarning
 class ProxiesSettings(BaseModel):
     type: Literal["http", "sock4", "sock5"]
---
+++ utils/start.py
@@ -0,0 +1,8 @@
+import os
+
+
+
+if os.path.exists("./logs/selenium/"):
+    pass
+else:
+    os.makedirs("./logs/selenium/")
