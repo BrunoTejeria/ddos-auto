ce4e45845602a768feb575057c16c707e06bb124
Bruno Tejeria <brunotejeria@hotmail.com>
2024-03-26 18:13:27 -0300

    ipv6 Terminar

--- utils/validate.py
+++ utils/validate.py
@@ -3,10 +3,8 @@ import re
 class ipv4():
     def __new__(cls) -> bool:
         pattern = r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$"
-
         if re.match(pattern, cls):
             componentes = cls.split(".")
-
             for componente in componentes:
                 if not 0 <= int(componente) <= 255:
                     return False
@@ -16,4 +14,12 @@ class ipv4():
 
 class ipv6:
     def __new__(cls) -> bool:
-        ...
+        pattern = r""
+        if re.match(pattern, cls):
+            componentes = cls.split(":")
+            for componente in componentes:
+                if ...:
+                    return False
+            return True
+        else:
+            return False
