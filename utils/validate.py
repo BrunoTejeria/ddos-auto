82be475df0b412160ea79af6d144606923be23f6
Bruno Tejeria <brunotejeria@hotmail.com>
2024-04-03 07:05:15 -0300

    Cambiar __new__ port __call__ en validate.py

--- utils/validate.py
+++ utils/validate.py
@@ -2,10 +2,10 @@ import re
 
 class ipv4:
     @staticmethod
-    def __new__(cls, ip_address) -> bool:
+    def __call__(cls, ip_address) -> bool:
         pattern = r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$"
         if re.match(pattern, ip_address):
-            components = ip_address.split(".")
+            components: int = ip_address.split(".")
             for component in components:
                 if not 0 <= int(component) <= 255:
                     return False
@@ -15,7 +15,7 @@ class ipv4:
 
 class ipv6:
     @staticmethod
-    def __new__(cls, ip_address) -> bool:
+    def __call__(cls, ip_address) -> bool:
         pattern = r"^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$"
         if re.match(pattern, ip_address, re.IGNORECASE):
             return True
@@ -24,7 +24,7 @@ class ipv6:
 
 class port:
     @staticmethod
-    def __new__(cls, port_number) -> bool:
+    def __call__(cls, port_number) -> bool:
         if 0 <= port_number <= 65535:
             return True
         else:
